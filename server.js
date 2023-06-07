b273bba2d538e30b4e8534168d0b919ac71bb913
Pedro Fernandes <thephfernandes@gmail.com>
2023-06-07 10:33:49 -0300

    revert

--- server.js
+++ server.js
@@ -53,11 +53,6 @@ app.use((req, res, next) => {
 });
 app.use(cors(corsOptions));
 
-logger.info(`Environment target ${process.env.NODE_ENV}`);
-logger.info(process.env.NODE_ENV !== 'development' ? 'turbopartners.com.br' : "");
-logger.info(process.env.NODE_ENV !== 'development');
-logger.info(process.env.NODE_ENV !== 'development' ? 'none' : 'lax');
-
 app.use(session({
 	store: MongoStore.create(
 		{
@@ -71,7 +66,7 @@ app.use(session({
 	resave: false,
 	saveUninitialized: false,
 	cookie: {
-		domain: process.env.NODE_ENV !== 'development' ? 'elasticbeanstalk.com' : "",
+		domain: process.env.NODE_ENV !== 'development' ? 'turbopartners.com.br' : "",
 		secure: process.env.NODE_ENV !== 'development',
 		sameSite: process.env.NODE_ENV !== 'development' ? 'none' : 'lax'
 	}
