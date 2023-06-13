a8dcda78ee55632f92e827adb5b9b05b082ea470
Pedro Fernandes <thephfernandes@gmail.com>
2023-06-13 15:24:55 -0300

    removes fake cookie

--- routes/auth.js
+++ routes/auth.js
@@ -43,7 +43,6 @@ router.post('/auth/login', async (req, res) => {
 	}
 
 	await logIn(req, found.id);
-	res.setHeader("Set-Cookie", "tech=shark; Path=/; HttpOnly");
 	res.json({ success: true, message: "User logged in" });
 });
 
